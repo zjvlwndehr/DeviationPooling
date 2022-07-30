213ac773d979cf0bc8c9023dfa8613a223aa276e
TaehyeonPark <53563433+zjvlwndehr@users.noreply.github.com>
2022-07-30 14:56:48 +0900

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -6,47 +6,57 @@ import glob
 
 class algo:
     def __init__(self):
+        # envirionment variable
         self.root = os.getenv('DATASET_PATH')
+        # for saving image
         self.mode = ''        
+        # original mnist dataset
         self.path = self.root + '\\mnist\\*\\*.png'
 
         print(self.path)
 
         self.path = glob.glob(self.path)
-
+        # shuffle
         self.path = np.random.permutation(self.path)
 
-        self.path = self.path[:100]
+        # pick up first 1000 images from mnist(60000)
+        self.path = self.path[:1000]
+
         print(self.path.shape)
+        # load image
         print('start loading data...')        
         self.x_indep = np.array([cv2.imread(i, cv2.IMREAD_GRAYSCALE) for i in self.path])
         self.y_indep = np.array([int(i.split('\\')[-2]) for i in self.path])
         print('data loaded')
 
     def start(self):
+        # original image information
         print('### original image ###')
         print(self.x_indep.shape)
         print(self.x_indep.dtype)
         print(self.x_indep.ndim)
-
+        
+        # deviation pooling
         print('### deviation pooling ###')
         cnt = 0
         self.mode = 'd_pool'
         for data in self.x_indep:
             # data: (array), filter_size: (n, n), stride: (n, n)
-            self.DeviationPooling2D(data, (2, 2), (1, 1), cnt)
+            self.DeviationPooling2D(data, (2, 2), (1, 1), cnt) # FILTER (2, 2), STRIDE(1, 1)
             cnt += 1
         cnt = 0
         
+        # max pooling
         print('### max pooling ###')
         cnt = 0
         self.mode = 'm_pool'
         for data in self.x_indep:
             # data: (array), filter_size: (n, n), stride: (n, n)
-            self.MaxPooling2D(data, (2, 2), (1, 1), cnt)
+            self.MaxPooling2D(data, (2, 2), (1, 1), cnt) # FILTER (2, 2), STRIDE(1, 1)
             cnt += 1
         cnt = 0
 
+    # data: (array), name: (str), id: (int)
     def save_as_image(self, data, name, id):
         cv2.imwrite(f'./{self.mode}/{self.y_indep[id]}/{name}.png', data)
         print(f'./{self.mode}/{self.y_indep[id]}/{name}.png')
@@ -58,13 +68,16 @@ class algo:
         filter_width, filter_height = filter_size
         stride_width, stride_height = stride
 
+        # calculate pooling size
         pool_width_size = int((width - filter_width) / stride_width) + 1
         pool_height_size = int((height - filter_height) / stride_height) + 1
 
+        # create output array
         output = np.zeros(
             (pool_width_size, pool_height_size), dtype=data.dtype)
         print('output shape: ', output.shape)
 
+        # pooling
         for stride_i in range(0, pool_height_size):
             i = stride_i * stride_height
             for stride_j in range(0, pool_width_size):
@@ -86,13 +99,16 @@ class algo:
         filter_width, filter_height = filter_size
         stride_width, stride_height = stride
 
+        # calculate pooling size
         pool_width_size = int((width - filter_width) / stride_width) + 1
         pool_height_size = int((height - filter_height) / stride_height) + 1
 
+        # create output array
         output = np.zeros(
             (pool_width_size, pool_height_size), dtype=data.dtype)
         print('output shape: ', output.shape)
 
+        # pooling
         for stride_i in range(0, pool_height_size):
             i = stride_i * stride_height
             for stride_j in range(0, pool_width_size):
